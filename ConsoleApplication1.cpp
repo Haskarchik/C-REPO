4f45f67dd5a3e73dcd6109c619733c553fdfc7c4
Volodya <55237974+Haskarchik@users.noreply.github.com>
2020-02-19 22:37:45 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+void doMath(int a)
+{
+    int b = 5;
+    std::cout << "doMath: a = " << a << " and b = " << b << std::endl;
+    a = 4;
+    std::cout << "doMath: a = " << a << " and b = " << b << std::endl;
+}
+
+int main()
+{
+    int a = 6;
+    int b = 7;
+    std::cout << "main: a = " << a << " and b = " << b << std::endl;
+    doMath(a);
+    std::cout << "main: a = " << a << " and b = " << b << std::endl;
+    return 0;
+}char(a);
